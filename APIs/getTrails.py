cb2e09a7ff38c65dd45304513d421b47f2c6adf1
mkhalil7625@gmail.com <mkhalil7625@gmail.com>
2019-03-25 00:31:23 -0500

    set max number of trails to 5 instead of 10

--- APIs/getTrails.py
+++ APIs/getTrails.py
@@ -4,7 +4,7 @@ import APIs.geocode as geoCode
 
 def get_trail(lat, lon):
     key = os.environ.get('TRAIL_KEY')
-    query = {'lat':lat, 'lon':lon, 'key':key, 'maxDistance':10}
+    query = {'lat':lat, 'lon':lon, 'key':key, 'maxDistance':10, 'maxResults':5}
     url = 'https://www.hikingproject.com/data/get-trails'
     data = requests.get(url, params=query).json()
 
@@ -18,6 +18,11 @@ def get_trail(lat, lon):
         print(name)
         summary = trail['summary']
         stars = trail['stars']
+        print(stars)
+        starVotes = trail['starVotes']
+        print(starVotes)
+        image_url = trail['imgMedium']
+        length = trail['length']
 
 
 
