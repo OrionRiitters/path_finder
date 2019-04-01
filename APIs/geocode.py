730afc60403afc3e754675a01f60111cc3b32436
Qaalib Farah <[si8822fb@gmail.com]>
2019-04-01 16:00:53 -0500

    resolved problem where database model would not serialize into json. Now client can successfully get bucket list from the database

--- APIs/geocode.py
+++ APIs/geocode.py
@@ -9,7 +9,5 @@ def geocode(city, state):
     data = requests.get(url, params=query).json()
     lat = data[0]['lat']
     long = data[0]['lon']
-    print(lat, long)
 
     return lat, long
-geocode('maple grove', 'mn')
