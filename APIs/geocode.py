2df761f0ed2e4e278b3d28009c6cfbc07d4454cd
mkhalil7625@gmail.com <mkhalil7625@gmail.com>
2019-04-01 08:01:53 -0500

    cashed the requests and added try exceot blocks

--- APIs/geocode.py
+++ APIs/geocode.py
@@ -1,15 +1,26 @@
 import requests
+import requests_cache
+
+requests_cache.install_cache()
 
 def geocode(city, state):
 
     # encodedCity = city.replace(' ', '%20')
 
-    url = 'https://nominatim.openstreetmap.org/search'
-    query = {'format':'json','city':city, 'state':state}
-    data = requests.get(url, params=query).json()
-    lat = data[0]['lat']
-    long = data[0]['lon']
-    print(lat, long)
-
+    try:
+        url = 'https://nominatim.openstreetmap.org/search'
+        query = {'format':'json','city':city, 'state':state}
+        data = requests.get(url, params=query).json()
+        lat = data[0]['lat']
+        long = data[0]['lon']
+        print(lat, long)
+    except requests.exceptions.HTTPError as http_error:
+        print("There's a Http Error", http_error)
+    except requests.exceptions.ConnectionError as conn:
+        print("There's a connection error", conn)
+    except requests.exceptions.Timeout as timeout:
+        print("There is a timeout Error", timeout)
+    except requests.exceptions.RequestException as error:
+        print("Something went wrong", error)
     return lat, long
-geocode('maple grove', 'mn')
+# geocode('maple grove', 'mn')
--- APIs/getTrails.py
+++ APIs/getTrails.py
@@ -1,11 +1,23 @@
 import requests
 import os
-import APIs.geocode as geoCode
+
+import requests_cache
+
+requests_cache.install_cache()
 
 def get_trail(lat, lon):
     key = os.environ.get('TRAIL_KEY')
-    query = {'lat':lat, 'lon':lon, 'maxDistance':10, 'maxResults':5, 'key': key}
-    url = 'https://www.hikingproject.com/data/get-trails'
+    try:
+        query = {'lat':lat, 'lon':lon, 'maxDistance':10, 'maxResults':5, 'key': key}
+        url = 'https://www.hikingproject.com/data/get-trails'
 
-    data = requests.get(url, params=query).json()
+        data = requests.get(url, params=query).json()
+    except requests.exceptions.HTTPError as http_error:
+        print("There's a Http Error", http_error)
+    except requests.exceptions.ConnectionError as conn:
+        print("There's a connection error", conn)
+    except requests.exceptions.Timeout as timeout:
+        print("There is a timeout Error", timeout)
+    except requests.exceptions.RequestException as error:
+        print("Something went wrong", error)
     return data
